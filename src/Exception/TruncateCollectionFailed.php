841f40ade0b956de3b2308401347629739c46cbf
Sandro Keil <github@sandro-keil.de>
2017-07-20 22:25:11 +0200

    Update to latest snapshot store implementation (close #9)

---
+++ src/Exception/TruncateCollectionFailed.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the prooph/arangodb-snapshot-store.
+ * (c) 2016-2017 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2017 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Prooph\ArangoDB\SnapshotStore\Exception;
+
+use ArangoDBClient\ServerException;
+
+class TruncateCollectionFailed extends RuntimeException
+{
+    /**
+     * Server exception
+     *
+     * @var ServerException
+     */
+    private $serverException;
+
+    public static function with(string $collectionName, ServerException $ex)
+    {
+        $self = new self(
+            sprintf(
+                'Could not truncate "%s". Got HTTP status %s - %s',
+                $collectionName,
+                $ex->getServerCode(),
+                $ex->getServerMessage()
+            )
+        );
+
+        return $self;
+    }
+
+    public function getServerException(): ServerException
+    {
+        return $this->serverException;
+    }
+}
